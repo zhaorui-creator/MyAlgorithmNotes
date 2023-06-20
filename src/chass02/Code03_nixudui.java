2e0cbc65cc47372018ac4f946c105eb66f249194
ZR <zhaoruisunshine@163.com>
2023-06-20 21:49:48 +0800

    逆序对问题

---
+++ src/chass02/Code03_nixudui.java
@@ -0,0 +1,14 @@
+package chass02;
+
+/**
+ * @author zhaorui
+ * @version 1.0
+ */
+
+/**
+ * 逆序对问题
+ * 在一个数组中，左边的数比右边的大,则这两个数构成一个逆序对,请返回逆序对的数量 同小和问题
+ * TODO  代码完善
+ */
+public class Code03_nixudui {
+}
