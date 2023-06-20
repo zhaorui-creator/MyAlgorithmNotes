7cca80d9699170a9a0be97b5a783601ecf3119e1
ZR <zhaoruisunshine@163.com>
2023-06-20 20:09:43 +0800

    归并排序

---
+++ src/chass02/Code01_MergeSort.java
@@ -0,0 +1,58 @@
+package chass02;
+
+import java.util.Arrays;
+
+/**
+ * @author zhaorui
+ * @version 1.0
+ */
+public class Code01_MergeSort {
+    public static void mergeSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process(arr, 0, arr.length - 1);
+    }
+
+
+    //请把arr[L..R]上排好序
+    public static void process(int[] arr, int L, int R) {
+        if (L == R) {
+            return;
+        }
+
+        int mid = L + ((R - L) >> 1);
+        process(arr, L, mid);
+        process(arr, mid, R);
+        merge(arr, L, mid, R);
+    }
+
+    public static void merge(int[] arr, int L, int M, int R) {
+        int[] help = new int[R - L + 1];
+        int i = 0;
+        int p1 = L;
+        int p2 = M + 1;
+        while (p1 <= M && p2 <= R) {
+            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
+        }
+
+        while (p1 <= M) {
+            help[i++] = arr[p1++];
+        }
+
+        while (p2 <= R) {
+            help[i++] = arr[p2++];
+        }
+
+        for (i = 0; i < arr.length; i++) {
+            arr[L + i] = help[i];
+        }
+    }
+
+    public static void main(String[] args){
+
+
+    }
+}
+
+
